af2f2534dbb165d812144138604a692d381b3dcc
copilotmediagroup <copilotmediagroup@gmail.com>
2026-06-19 11:36:07 -0700

    Add files via upload

--- public/config.js
+++ public/config.js
@@ -1,4 +1,5 @@
 // Co Pilot Collections Manager - LIVE Supabase Config
+// IMPORTANT: use base project URL only. Do NOT include /rest/v1/.
 window.CO_PILOT_SUPABASE_CONFIG = {
   url: "https://bwvufgzbkaymffwxuuzr.supabase.co",
   publishableKey: "sb_publishable_cvwkN3fOovsYvAvzXvrmRA_TKauXT_d"
